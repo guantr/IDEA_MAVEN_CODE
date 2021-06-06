1a1ee47c10deac908cd1bc698cd7363272d1b502
guantr <angel745279496@163.com>
2021-06-06 16:12:06 +0800

    first commit

---
+++ src/test/java/com/guan/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.guan;
+
+/**
+ * @author xiyi
+ * @date 2021/6/6 - 16:01
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
